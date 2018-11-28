baf2b0bd169680f9196f72c0a115050588bbfddb
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-11-27 20:21:07 -0500

    Began adding ChooseGameActivity, IntroductionActivity and NewUserActivity, and wired them up to the appropriate buttons

---
+++ Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/IntroductionActivity.java
@@ -0,0 +1,16 @@
+package fall18project.gamecentre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Give a user the introduction to In the Colonel, and then modify the user settings appropriately
+ */
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+    }
+}
---
+++ Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/user_management/NewUserActivity.java
@@ -0,0 +1,18 @@
+package fall18project.gamecentre.user_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall18project.gamecentre.R;
+
+/**
+ * Activity to create a new user
+ */
+public class NewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}
