f4c9a3d2e3a1cef8b8012a5cdce0b0afb0209d47
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-11-28 22:52:00 -0500

    Removed MVC JumpingRabbit code

--- Phase2/JumpingRabbit/app/src/main/java/com/example/lenovo/PoisonTargetModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.lenovo;
-
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-
-public class PoisonTargetModel extends TargetModel {
-    private static int velocity = 30;
-}
