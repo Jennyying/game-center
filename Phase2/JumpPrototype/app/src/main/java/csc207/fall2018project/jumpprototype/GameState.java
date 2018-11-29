69fd8b962be8c7d3d78f648bee3ad20be4edf7ad
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-11-29 00:38:02 -0500

    Began adding GameState and PlayerState classes to JumpingRabbit with Jenny

---
+++ Phase2/JumpPrototype/app/src/main/java/csc207/fall2018project/jumpprototype/GameState.java
@@ -0,0 +1,10 @@
+package csc207.fall2018project.jumpprototype;
+
+/**
+ * The state of a JumpingRabbit game, including:
+ * - Whether the game is paused or running
+ * - The position and velocity of the rabbit and of any obstacles
+ */
+public class GameState {
+
+}
