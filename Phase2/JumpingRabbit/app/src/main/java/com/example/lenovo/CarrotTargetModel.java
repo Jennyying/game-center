ee586f2e5936a05a8deb2c20ddb5d33688fe3c88
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-11-28 22:27:30 -0500

    Added in new MVC pattern, still not working code for JumpingRabbit from Jenny

---
+++ Phase2/JumpingRabbit/app/src/main/java/com/example/lenovo/CarrotTargetModel.java
@@ -0,0 +1,12 @@
+package com.example.lenovo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+import com.example.lenovo.jump2.R;
+
+
+public class CarrotTargetModel extends TargetModel {
+    private static int velocity = 20;
+}
---
+++ Phase2/JumpingRabbit/app/src/main/java/com/example/lenovo/TargetModel.java
@@ -0,0 +1,26 @@
+package com.example.lenovo;
+
+import android.graphics.Bitmap;
+
+/**The superclass of target the rabbit is aim at in the game.
+ *
+ */
+
+public class TargetModel {
+
+    //the field of TargetModel
+    protected static Bitmap image;
+
+    public static int getVelocity() {
+        return velocity;
+    }
+
+    protected static int velocity;
+    protected static int imageX;
+    protected static int imageY;
+
+    public static void setImage(Bitmap image) {
+        TargetModel.image = image;
+    }
+
+}
