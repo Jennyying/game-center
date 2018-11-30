a167234f68a49b72f35ef7e62c87757a0696b206
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-11-29 22:31:54 -0500

    Fixed LoginManager bugs, began implementing login interface

--- Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/user_management/LoginActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/user_management/LoginActivity.java
@@ -4,6 +4,12 @@ import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 import fall18project.gamecentre.R;
 import fall18project.gamecentre.user_management.User;
@@ -15,14 +21,38 @@ import fall18project.gamecentre.utilities.WeatherBackground;
 public class LoginActivity extends AppCompatActivity {
 
     /**
-     * User management system
-     */
-    private UserManager userManager;
-
-    /**
      * Login management system
      */
     private LoginManager loginManager;
+
+    /**
+     * A list of all usernames in the login management system. We can compute this on creation, and
+     * re-compute on resumption, since we know that while this activity is open the list of usernames
+     * will not changed
+     */
+    private List<String> usernameList;
+
+    /**
+     * Compute the list of usernames in the login manager
+     */
+    private void computeUsernameList() {
+        usernameList = loginManager.getUserNameList();
+    }
+
+    /**
+     * Username field
+     */
+    private AutoCompleteTextView userNameField;
+
+    /**
+     * Password field
+     */
+    private EditText passwordField;
+
+    /**
+     * Login button
+     */
+    private Button loginButton;
 
     /**
      * Background graphics manager (for rain effect and gradient animation)
@@ -34,17 +64,37 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userManager = new UserManager(this, UserManager.DEFAULT_USER_PREFIX);
         loginManager = new LoginManager(this, LoginManager.DEFAULT_FILE_NAME);
+        computeUsernameList();
 
+        setUpInterface();
+    }
+
+    private void setUpInterface() {
         ConstraintLayout layout = findViewById(R.id.constraintLayout);
         background = new WeatherBackground(this, null, layout);
+
+        loginButton = findViewById(R.id.login);
+        userNameField = findViewById(R.id.username);
+        passwordField = findViewById(R.id.password);
+
+        setUpUsernameFieldAutocomplete();
+    }
+
+    /**
+     * Set up autocompletion for the username field
+     */
+    private void setUpUsernameFieldAutocomplete() {
+        userNameField.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                usernameList));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         background.onResume();
+        computeUsernameList();
     }
 
     @Override
--- Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/user_management/LoginManager.java
+++ Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/user_management/LoginManager.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -25,101 +26,6 @@ import fall18project.gamecentre.utilities.Saveable;
  */
 public class LoginManager implements Saveable {
 
-    class SaltAndDigest {
-
-        private static final int DEFAULT_SALT_BYTES = 8;
-
-        private byte[] salt;
-        private byte[] digest;
-
-        /**
-         * Create a salt and digest pair with the given salt and digest
-         * @param salt the salt to use, which can be of any length, though 8 bytes is usually used here
-         * @param digest the resulting message digest to be stored with the salt
-         */
-        public SaltAndDigest(byte[] salt, byte[] digest) {
-            this.salt = salt;
-            this.digest = digest;
-        }
-
-        /**
-         * Digest a message with the given salt using the SHA256 algorithm
-         * @param salt salt to use (as prefix)
-         * @param message message to digest
-         * @return the SHA256 digest of the given message prefixed by the given salt
-         */
-        public byte[] getDigest(byte[] salt, String message) {
-            try {
-                MessageDigest digester = MessageDigest.getInstance("SHA-256");
-                digester.update(salt);
-                return digester.digest(message.getBytes());
-            } catch(NoSuchAlgorithmException e) {
-                throw new RuntimeException("Error initializing message digester " + e.toString());
-            }
-        }
-
-        /**
-         * Digest a message with the salt contained in this object using the SHA256 algorithm
-         * @param message message to digest
-         * @return SHA256 hash of message with the salt in this object
-         */
-        public byte[] getDigest(String message) {
-            return getDigest(this.salt, message);
-        }
-
-        /**
-         * Digest a message with the given salt using the SHA256 algorithm and set this object to
-         * the given salt, digest pair
-         * @param salt salt to use (as prefix)
-         * @param message message to digest
-         */
-        private void setDigest(byte[] salt, String message) {
-            this.salt = salt;
-            this.digest = getDigest(salt, message);
-        }
-
-        /**
-         * Store the digest of the given message with the given salt in this object
-         * @param salt salt to use (as prefix)
-         * @param message message to digest
-         */
-        public SaltAndDigest(byte[] salt, String message) {
-            setDigest(salt, message);
-        }
-
-        /**
-         * Digest a message with a random hash using the SHA256 algorithm and store it in this
-         * SaltAndHash object. The salt will be DEFAULT_SALT_BYTES long
-         * @param message message to digest
-         */
-        public SaltAndDigest(String message) {
-            SecureRandom rng = new SecureRandom();
-            byte[] salt = new byte[DEFAULT_SALT_BYTES];
-            rng.nextBytes(salt);
-            setDigest(salt, message);
-        }
-
-        /**
-         * See whether another digest is equivalent to this one
-         * @param digest message digest to compare
-         * @return whether it is equivalent to the stored digest
-         */
-        public boolean equalDigest(byte[] digest) {
-            return Arrays.equals(this.digest, digest);
-        }
-
-        /**
-         * See whether a message salted with this object's salt gives the same digest as stored in
-         * this object
-         * @param message message to check
-         * @return whether the digest of this message with the salt in this object is the same as
-         * this object's digest
-         */
-        public boolean equalMessage(String message) {
-            return equalDigest(getDigest(message));
-        }
-    }
-
     /**
      * The default file name for the login manager
      */
@@ -129,7 +35,7 @@ public class LoginManager implements Saveable {
      * A database mapping strings to byte arrays representing the salted SHA256 message
      * digests of user passwords along with a salt
      */
-    private Map<String, SaltAndDigest> passwordDatabase = new HashMap<>();
+    private Map<String, SaltAndDigest> passwordDatabase;
 
     /**
      * The context to use to open files. May be null, in which case file operations will throw exceptions
@@ -150,7 +56,8 @@ public class LoginManager implements Saveable {
     public LoginManager(Context context, String fileName) {
         this.context = context;
         this.fileName = fileName;
-        Map<String, SaltAndDigest> lm = loadMapFromFile();
+        passwordDatabase = loadMapFromFile();
+        if(passwordDatabase == null) passwordDatabase = new HashMap<>();
     }
 
     /**
