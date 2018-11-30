95aa71423a5094ac68dd067ce414ed7eda112af5
karanbrar <karanbirsingh.brar@mail.utoronto.ca>
2018-11-29 22:38:30 -0500

    Updated class TileView

--- Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/minesweeper/TileView.java
+++ Phase2/GameCentre/app/src/main/java/fall18project/gamecentre/minesweeper/TileView.java
@@ -2,9 +2,11 @@ package fall18project.gamecentre.minesweeper;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.LevelListDrawable;
 import android.view.View;
-
+import fall18project.gamecentre.R;
 import com.squareup.otto.Bus;
 
 import java.util.HashMap;
@@ -17,7 +19,7 @@ public class TileView extends View {
      * The Tile states.
      */
     public static final int COVERED = 0;
-    public static final int IS_MINE = 1;
+    public static final int FlAGGED_AS_MINE = 1;
     public static final int UNCOVERED = 2;
 
     /**
@@ -80,6 +82,76 @@ public class TileView extends View {
         this.yCoord = yCoord;
 
         gameBus = MainApp.getGameBus();
+        setupDrawableBackgrounds();
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Notify the game that the user has performed an action on this tile.
+                gameBus.post(new Game.TileViewActionEvent(TileView.this, CLICK));
+            }
+        });
+
+        setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                // Notify the game that the user has performed an action on this tile.
+                gameBus.post(new Game.TileViewActionEvent(TileView.this, LONG_CLICK));
+
+                // Return true to consume event.
+                return true;
+            }
+        });
+    }
+
+    private void setupDrawableBackgrounds() {
+        Drawable coveredTile = setupCoveredTile();
+        LayerDrawable flaggedMineDrawable = new LayerDrawable(new Drawable[]{coveredTile, new ConcentricCirclesDrawable()});
+
+        drawableContainer = new LevelListDrawable();
+        drawableContainer.addLevel(0, COVERED, coveredTile);
+        drawableContainer.addLevel(0, FlAGGED_AS_MINE, flaggedMineDrawable);
+
+        setBackground(drawableContainer);
+    }
+
+    private Drawable setupCoveredTile()  {
+        int colorInner = Graphics.getColor(getContext(), R.color.blue_grey_200);
+        int colorTop = Graphics.getColor(getContext(), R.color.blue_grey_300);
+        int colorLeft = Graphics.getColor(getContext(), R.color.blue_grey_400);
+        int colorBottom = Graphics.getColor(getContext(), R.color.blue_grey_500);
+        int colorRight = Graphics.getColor(getContext(), R.color.blue_grey_600);
+
+        int[] tileColors = new int[]{colorInner, colorLeft, colorTop, colorRight, colorBottom};
+
+        return new TileDrawable(tileColors, null);
+    }
+
+    public void setupUncoveredTileDrawable(Tile tile) {
+        Drawable uncoveredDrawable;
+
+        if(tile != null && tile.containsMine()) {
+            uncoveredDrawable = new ConcentricCirclesDrawable(new int[]{Color.RED, Color.BLACK}, 0.50f);
+        }
+        else {
+            String adjacentMineCountText;
+            int textColor = 0;
+
+            if(tile == null) {
+                adjacentMineCountText = "";
+            }
+            else {
+                int adjacentMinesCount = tile.getAdjacentMines();
+
+                textColor = adjacentMinesToColor.get(adjacentMinesCount);
+                adjacentMineCountText = String.valueOf(adjacentMinesCount);
+            }
+            uncoveredDrawable = new TextDrawable(adjacentMineCountText, textColor);
+        }
+        drawableContainer.addLevel(0, UNCOVERED, uncoveredDrawable);
     }
 
     /**
